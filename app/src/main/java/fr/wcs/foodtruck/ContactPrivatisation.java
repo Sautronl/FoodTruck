00352c986744d32571a3e479450367adc01ff820
Sam <sam64mc@gmail.com>
2017-09-27 19:08:15 +0200

    page privatisation & contact squelette

---
+++ app/src/main/java/fr/wcs/foodtruck/ContactPrivatisation.java
@@ -0,0 +1,100 @@
+package fr.wcs.foodtruck;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by sam on 26/09/17.
+ */
+
+public class ContactPrivatisation extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contactprivatisation);
+
+        EditText editSujet = (EditText) findViewById(R.id.sujet);
+
+
+        // checkbox
+        final CheckBox checkbox = (CheckBox) findViewById(R.id.checkbox);
+        checkbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editSujet= (EditText) findViewById(R.id.sujet);
+
+                if (checkbox.isChecked()) {
+                    // est coché
+                    String privateOK = editSujet.getResources().getString(R.string.sujet2);
+                    editSujet.setText(privateOK);
+
+                } else {
+                    // n'est pas coché
+                    editSujet.setText("");
+                }
+            }
+        });
+
+        Intent intent = getIntent();
+
+        // bascule sur la page menu en cliquant sur le logo menu
+
+        ImageButton boutonRetourMenu = (ImageButton) findViewById(R.id.boutonRetourMenu);
+        final Intent intent2 = new Intent(ContactPrivatisation.this,MainActivity.class);
+        boutonRetourMenu.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startActivity(intent2);
+            }
+        });
+
+          // Message Toast si les champs obligatoires ne sont pas remplis
+
+        Button boutonValider = (Button)findViewById(R.id.boutonEnvoyer);
+        boutonValider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editPrenomNom = (EditText) findViewById(R.id.prenomNom);
+                EditText editSujet = (EditText)findViewById(R.id.sujet);
+                EditText editMessage = (EditText)findViewById(R.id.message);
+                if(editPrenomNom.getText().toString().isEmpty()
+                        || editSujet.getText().toString().isEmpty()
+                        || editMessage.getText().toString().isEmpty() )
+                {
+
+                    CharSequence text = getResources().getString(R.string.messToast);
+                    int duration = Toast.LENGTH_SHORT;
+                    Context context = getApplicationContext();
+                    Toast messToast = Toast.makeText(context, text, duration);
+                    messToast.show();
+                }
+             // Message Toast de confirmation d'envoie
+                else {
+                    CharSequence text = getResources().getString(R.string.messToastValider);
+                    int duration = Toast.LENGTH_SHORT;
+                    Context context = getApplicationContext();
+                    Toast messToast = Toast.makeText(context, text, duration);
+                    messToast.show(); }
+
+            }
+
+
+
+        });
+
+
+
+    }
+
+}
--- app/src/main/java/fr/wcs/foodtruck/MainActivity.java
+++ app/src/main/java/fr/wcs/foodtruck/MainActivity.java
@@ -1,7 +1,13 @@
 package fr.wcs.foodtruck;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import static fr.wcs.foodtruck.R.id.boutonTest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent2 = getIntent();
+
+        final Button boutonTest = (Button) findViewById(R.id.boutonTest);
+
+        boutonTest.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ContactPrivatisation.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
