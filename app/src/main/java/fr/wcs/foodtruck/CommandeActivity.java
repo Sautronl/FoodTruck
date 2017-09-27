af57d26272d33f74af77d0b6e039334755794c97
Cozart98 <chihaoui1098@gmail.com>
2017-09-27 13:44:36 +0200

    element de la page Commancer placer dans le layout

--- app/src/main/java/fr/wcs/foodtruck/CommandeActivity.java
+++ app/src/main/java/fr/wcs/foodtruck/CommandeActivity.java
@@ -3,11 +3,11 @@ package fr.wcs.foodtruck;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CommandeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_commande);
     }
 }
