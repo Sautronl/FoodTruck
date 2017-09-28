0d350d10ccb206e080687207278afd324d38b464
Nono <lombardo.nora@gmail.com>
2017-09-28 15:35:17 +0200

    Creation vue menu du jour

--- app/src/main/java/fr/wcs/foodtruck/MainActivity.java
+++ app/src/main/java/fr/wcs/foodtruck/MainActivity.java
@@ -3,6 +3,8 @@ package fr.wcs.foodtruck;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
