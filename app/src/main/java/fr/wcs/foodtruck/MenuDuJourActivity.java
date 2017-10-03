b4b4a1e70ba26cb7d3b870107c2fb6b8cee2d3b0
Nono <lombardo.nora@gmail.com>
2017-10-03 15:26:22 +0200

    modif page menu du jour

--- app/src/main/java/fr/wcs/foodtruck/MenuDuJourActivity.java
+++ app/src/main/java/fr/wcs/foodtruck/MenuDuJourActivity.java
@@ -1,9 +1,12 @@
 package fr.wcs.foodtruck;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -13,6 +16,7 @@ public class MenuDuJourActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_du_jour);
+
         Button reserver = (Button) findViewById(R.id.reserver);
         ImageView back = (ImageView) findViewById(R.id.backHomePage);
 
@@ -24,13 +28,13 @@ public class MenuDuJourActivity extends AppCompatActivity {
             }
         });
 
-        back.setOnClickListener(new View.OnClickListener() {
+        /*back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent backhome = new Intent(MenuDuJourActivity.this, MainActivity.class);
                 startActivity(backhome);
 
             }
-        });
+        });*/
     }
 }
