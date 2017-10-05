1d27f18ffba1bfda23b774ae9c621d68e9afecaa
Sautronl <sautronl31@gmail.com>
2017-10-05 16:55:47 +0200

    Connexion Admin(Email/password)

--- app/src/main/java/fr/wcs/foodtruck/MenuDuJourActivity.java
+++ app/src/main/java/fr/wcs/foodtruck/MenuDuJourActivity.java
@@ -11,7 +11,18 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MenuDuJourActivity extends AppCompatActivity {
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference mRootReference = firebaseDatabase.getReference();
+    private DatabaseReference mChildReference = mRootReference.child("option1");
+    private  TextView mNomDuPlat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,9 @@ public class MenuDuJourActivity extends AppCompatActivity {
 
         Button reserver = (Button) findViewById(R.id.reserver);
         TextView decouvrez = (TextView) findViewById(R.id.totheformules);
+        mNomDuPlat = (TextView) findViewById(R.id.nomDuPlat);
+
+
 
         final Intent intent = new Intent(MenuDuJourActivity.this, Commande.class);
         reserver.setOnClickListener(new View.OnClickListener() {
@@ -37,5 +51,21 @@ public class MenuDuJourActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mChildReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String optionMenu = dataSnapshot.getValue(String.class);
+                mNomDuPlat.setText(optionMenu);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
