82f357bac4d1dda42b8538b90b4cd03411363d75
Sautronl <sautronl31@gmail.com>
2017-10-27 12:05:08 +0200

    backbutton

--- app/src/main/java/fr/wcs/foodtruck/MainActivity.java
+++ app/src/main/java/fr/wcs/foodtruck/MainActivity.java
@@ -108,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
                 // http://stackoverflow.com/a/24547437/1048340
                 uri = Uri.parse("fb://facewebmodal/f?href=" + url);
             }
-        } catch (PackageManager.NameNotFoundException ignored) {
+        } catch (PackageManager.NameNotFoundException ignored) {/home/apprenti/AndroidStudioProjects/AutoBoat
         }
         return new Intent(Intent.ACTION_VIEW, uri);
     }*/
