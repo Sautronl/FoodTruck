a4641cb628321b4811bac3166b438d98b9e482d8
Sautronl <sautronl31@gmail.com>
2018-01-17 16:39:17 +0100

    details event ok

--- app/src/main/java/fr/wcs/foodtruck/UI/Activity/User/DetailEventFragment.java
+++ app/src/main/java/fr/wcs/foodtruck/UI/Activity/User/DetailEventFragment.java
@@ -1,16 +1,25 @@
 package fr.wcs.foodtruck.UI.Activity.User;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import fr.wcs.foodtruck.Model.EventModel;
 import fr.wcs.foodtruck.R;
+import fr.wcs.foodtruck.Utils.Constant;
+import fr.wcs.foodtruck.Utils.SetTypeFace;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +28,8 @@ public class DetailEventFragment extends Fragment {
 
     private FirebaseDatabase mFire;
     private DatabaseReference mRef;
+    private EventModel mEvent;
+    TextView mDesc,mTitre,mDate;
 
     public DetailEventFragment() {
         // Required empty public constructor
@@ -31,13 +42,36 @@ public class DetailEventFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_detail_event, container, false);
 
+        getActivity().setTitle("Détail de l'événement");
+
         mFire = FirebaseDatabase.getInstance();
         mRef = mFire.getReference("events");
 
+        RelativeLayout relativeEventUserDetail = view.findViewById(R.id.relativeEventUserDetail);
+
+        Typeface mainfont = Typeface.createFromAsset(getActivity().getAssets(), Constant.GOTHAM);
+        SetTypeFace.setAppFont(relativeEventUserDetail,mainfont);
+
         Bundle bundle = this.getArguments();
-        bundle.getParcelable("detailEvent");
+        mEvent = bundle.getParcelable("detailEvent");
 
+        mDesc = view.findViewById(R.id.main_desc);
+        mTitre = view.findViewById(R.id.titre_event_detail);
+        mDate = view.findViewById(R.id.date_detail);
 
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mTitre.setText(mEvent.getName());
+                mDate.setText(mEvent.getDate());
+                mDesc.setText(mEvent.getDetails());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         return view;
     }
