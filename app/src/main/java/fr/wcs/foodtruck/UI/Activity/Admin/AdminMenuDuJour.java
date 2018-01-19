8dcd7b5e2a6a8fae4d4a187618965e14d9bb20f7
Sautronl <sautronl31@gmail.com>
2018-01-19 15:37:13 +0100

    crop image OK

--- app/src/main/java/fr/wcs/foodtruck/UI/Activity/Admin/AdminMenuDuJour.java
+++ app/src/main/java/fr/wcs/foodtruck/UI/Activity/Admin/AdminMenuDuJour.java
@@ -2,10 +2,14 @@ package fr.wcs.foodtruck.UI.Activity.Admin;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +27,10 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.ByteArrayOutputStream;
 
 import fr.wcs.foodtruck.Model.MajPlatDuJour;
 import fr.wcs.foodtruck.R;
@@ -39,8 +47,11 @@ public class  AdminMenuDuJour extends AppCompatActivity {
     private ImageView mImgMenu;
     private StorageReference mStorage;
     private static final int REQUEST_CODE = 20;
+    int day;
     private Uri imgUri;
+     MajPlatDuJour mMajPlatDuJour = null;
 
+    public static String TAG = "AdminMenuDuJour";
 
     private FirebaseDatabase mFireMenu;
     private DatabaseReference mDbRefMenu;
@@ -55,7 +66,7 @@ public class  AdminMenuDuJour extends AppCompatActivity {
         Typeface mainfont = Typeface.createFromAsset(getAssets(), Constant.GOTHAM);
         SetTypeFace.setAppFont(scrollMenuAdmin,mainfont);
 
-        int day;
+
         mNomPlatDuJour = (EditText) findViewById(R.id.nomDuPlat);
         mDescriptionDuPlat = (EditText) findViewById(R.id.descriPlat);
         mPrixDuPlat = (EditText) findViewById(R.id.prix);
@@ -67,84 +78,167 @@ public class  AdminMenuDuJour extends AppCompatActivity {
         mStorage = FirebaseStorage.getInstance().getReference();
         mImgMenu = (ImageView) findViewById(R.id.imgDuPlat);
 
+        mImgMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity()
+                        .setCropShape(CropImageView.CropShape.OVAL)
+                        .setAspectRatio(1, 1)
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(AdminMenuDuJour.this);
+            }
+        });
+
         Intent jour = getIntent();
         day = jour.getIntExtra("day", 0);
 
         if (day == 0) {
-            mDbRefMenu = mDbRefMenu.child("menu/menuLundi");
-            majClick();
-            addMaj();
+           // mDbRefMenu = mDbRefMenu.child("menu/menuLundi");
+            addMaj("menu/menuLundi/");
 
         } else if (day == 1) {
-            mDbRefMenu = mDbRefMenu.child("menu/menuMardi");
-            majClick();
-            addMaj();
+            //mDbRefMenu = mDbRefMenu.child("menu/menuMardi");
+            addMaj("menu/menuMardi/");
         }
         else if (day == 2) {
-            mDbRefMenu = mDbRefMenu.child("menu/menuMercredi");
-            majClick();
-            addMaj();
+            //mDbRefMenu = mDbRefMenu.child("menu/menuMercredi");
+            addMaj("menu/menuMercredi/");
         }
         else if (day == 3) {
-            mDbRefMenu = mDbRefMenu.child("menu/menuJeudi");
-            majClick();
-            addMaj();
+            //mDbRefMenu = mDbRefMenu.child("menu/menuJeudi");
+            addMaj("menu/menuJeudi/");
         }
         else if (day == 4) {
-            mDbRefMenu = mDbRefMenu.child("menu/menuVendredi");
-            majClick();
-            addMaj();
+           // mDbRefMenu = mDbRefMenu.child("menu/menuVendredi");
+            addMaj("menu/menuVendredi/");
         }
-    }
 
-    protected void majClick(){
         mMaj.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Selectionner une image"), REQUEST_CODE);
+                if (day == 0) {
+                    //mDbRefMenu = mDbRefMenu.child("menu/menuLundi/");
+                    firebaseInfoMenu("menu/menuLundi/");
+                     //addMaj("menu/menuLundi/");
+
+                } else if (day == 1) {
+                   // mDbRefMenu = mDbRefMenu.child("menu/menuMardi/");
+                    firebaseInfoMenu("menu/menuMardi/");
+                    //addMaj("menu/menuMardi/");
+                }
+                else if (day == 2) {
+                    //mDbRefMenu = mDbRefMenu.child("menu/menuMercredi/");
+                    firebaseInfoMenu("menu/menuMercredi/");
+                    //addMaj("menu/menuMercredi/");
+                }
+                else if (day == 3) {
+                    //mDbRefMenu = mDbRefMenu.child("menu/menuJeudi/");
+                    firebaseInfoMenu("menu/menuJeudi/");
+                    //addMaj("menu/menuJeudi/");
+                }
+                else if (day == 4) {
+                    //mDbRefMenu = mDbRefMenu.child("menu/menuVendredi/");
+                    firebaseInfoMenu("menu/menuVendredi/");
+                    //addMaj("menu/menuVendredi/");
+                }
             }
         });
     }
 
-     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    private void firebaseInfoMenu(String child){
+        mMajPlatDuJour.setNomPlat(mNomPlatDuJour.getText().toString());
+        mMajPlatDuJour.setDescPlat(mDescriptionDuPlat.getText().toString());
+        mMajPlatDuJour.setPrix(mPrixDuPlat.getText().toString());
 
-        if ((requestCode == REQUEST_CODE && resultCode == RESULT_OK) && data != null && data.getData() != null) {
+        mImgMenu.setDrawingCacheEnabled(true);
+        mImgMenu.buildDrawingCache();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        mStorage = storage.getReference("PlatDuJour/");
 
-            imgUri = data.getData();
+        Bitmap bitmap = mImgMenu.getDrawingCache();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
 
-            StorageReference ref = mStorage.child("image/").child(imgUri.getLastPathSegment());
-            ref.putFile(imgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+        UploadTask uploadTask = mStorage.putBytes(data);
+        uploadTask.addOnFailureListener(exception -> Log.d(TAG, "onFailure: " + exception.getLocalizedMessage())).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                mMajPlatDuJour.setUrlImg(downloadUrl.toString());
+                mDbRefMenu.child(child).setValue(mMajPlatDuJour);
+            }
+        });
+    }
 
-                    if (mDescriptionDuPlat.getText().toString().isEmpty() || mNomPlatDuJour.getText().toString().isEmpty()) {
-                        Toast.makeText(AdminMenuDuJour.this, getResources().getString(R.string.messToast), Toast.LENGTH_SHORT).show();
-                    }
-                        else{
-                        MajPlatDuJour maj = new MajPlatDuJour(mNomPlatDuJour.getText().toString(),
-                                mDescriptionDuPlat.getText().toString(), taskSnapshot.getDownloadUrl().toString(),mPrixDuPlat.getText().toString());
-                        mDbRefMenu.setValue(maj);
-                        Glide.with(AdminMenuDuJour.this).load(maj.getUrlImg()).into(mImgMenu);
-                        Toast.makeText(AdminMenuDuJour.this, "Upload", Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
+    //MajPlatDuJour maj = new MajPlatDuJour(mNomPlatDuJour.getText().toString(),
+//                                mDescriptionDuPlat.getText().toString(), taskSnapshot.getDownloadUrl().toString(),mPrixDuPlat.getText().toString());
+//                        mDbRefMenu.setValue(maj);
+//                        Glide.with(AdminMenuDuJour.this).load(maj.getUrlImg()).into(mImgMenu);
+
+//        mMaj.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent();
+//                intent.setType("image/*");
+//                intent.setAction(intent.ACTION_GET_CONTENT);
+//                startActivityForResult(Intent.createChooser(intent, "Selectionner une image"), REQUEST_CODE);
+//            }
+//        });
+
+
+//     @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if ((requestCode == REQUEST_CODE && resultCode == RESULT_OK) && data != null && data.getData() != null) {
+//
+//            imgUri = data.getData();
+//
+//            StorageReference ref = mStorage.child("image/").child(imgUri.getLastPathSegment());
+//            ref.putFile(imgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                @Override
+//                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//
+//                    if (mDescriptionDuPlat.getText().toString().isEmpty() || mNomPlatDuJour.getText().toString().isEmpty()) {
+//                        Toast.makeText(AdminMenuDuJour.this, getResources().getString(R.string.messToast), Toast.LENGTH_SHORT).show();
+//                    }
+//                        else{
+//                        MajPlatDuJour maj = new MajPlatDuJour(mNomPlatDuJour.getText().toString(),
+//                                mDescriptionDuPlat.getText().toString(), taskSnapshot.getDownloadUrl().toString(),mPrixDuPlat.getText().toString());
+//                        mDbRefMenu.setValue(maj);
+//                        Glide.with(AdminMenuDuJour.this).load(maj.getUrlImg()).into(mImgMenu);
+//                        Toast.makeText(AdminMenuDuJour.this, "Upload", Toast.LENGTH_LONG).show();
+//                    }
+//                }
+//            });
+//        }
+//    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                imgUri = result.getUri();
+                mImgMenu.setImageDrawable(Drawable.createFromPath(imgUri.getPath()));
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception exception = result.getError();
+                Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
-    protected void addMaj(){
-        mDbRefMenu.addValueEventListener(new ValueEventListener() {
+    protected void addMaj(String day){
+        mDbRefMenu.child(day).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                MajPlatDuJour majText = dataSnapshot.getValue(MajPlatDuJour.class);
-                mNomPlatDuJour.setText(majText.getNomPlat());
-                mDescriptionDuPlat.setText(majText.getDescPlat());
-                Glide.with(getApplicationContext()).load(majText.getUrlImg()).into(mImgMenu);
+                mMajPlatDuJour = dataSnapshot.getValue(MajPlatDuJour.class);
+                mNomPlatDuJour.setText(mMajPlatDuJour.getNomPlat());
+                mDescriptionDuPlat.setText(mMajPlatDuJour.getDescPlat());
+                mPrixDuPlat.setText(mMajPlatDuJour.getPrix());
+                Glide.with(getApplicationContext()).load(mMajPlatDuJour.getUrlImg()).into(mImgMenu);
             }
 
             @Override
