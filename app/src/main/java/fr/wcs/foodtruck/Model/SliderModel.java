1e61f1107141b3eac5d05800a70dd5fb6b998863
Sautronl <sautronl31@gmail.com>
2018-05-20 16:18:23 +0200

    'test'

---
+++ app/src/main/java/fr/wcs/foodtruck/Model/SliderModel.java
@@ -0,0 +1,28 @@
+package fr.wcs.foodtruck.Model;
+
+/**
+ * Created by Poto on 20/05/2018.
+ */
+
+public class SliderModel {
+
+    private String sliderOne;
+
+    public SliderModel(){
+
+    }
+
+    public SliderModel(String sliderOne){
+        this.sliderOne = sliderOne;
+    }
+
+
+    public String getSliderOne() {
+        return sliderOne;
+    }
+
+    public void setSliderOne(String sliderOne) {
+        this.sliderOne = sliderOne;
+    }
+
+}
