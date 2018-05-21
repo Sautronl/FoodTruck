ec5edcaf493d1e28a060c1b1455ed99b9b6f619a
Sautronl <sautronl31@gmail.com>
2018-05-21 19:04:58 +0200

    slider firebase

---
+++ app/src/main/java/fr/wcs/foodtruck/Model/SliderModel.java
@@ -0,0 +1,21 @@
+package fr.wcs.foodtruck.Model;
+
+public class SliderModel {
+    private String sliderUrl;
+
+    public SliderModel(){
+
+    }
+
+    public SliderModel(String sliderUrl){
+        this.sliderUrl = sliderUrl;
+    }
+
+    public String getSliderUrl() {
+        return sliderUrl;
+    }
+
+    public void setSliderUrl(String sliderUrl) {
+        this.sliderUrl = sliderUrl;
+    }
+}
