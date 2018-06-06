53e26974db15924aebba7d7bcde5ef5f1c4c063d
Sautronl <sautronl31@gmail.com>
2018-06-06 15:42:58 +0200

    inProgress

--- app/src/main/java/fr/wcs/foodtruck/UI/Activity/Admin/HoraireActivity.java
+++ app/src/main/java/fr/wcs/foodtruck/UI/Activity/Admin/HoraireActivity.java
@@ -1,10 +1,13 @@
 package fr.wcs.foodtruck.UI.Activity.Admin;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
 import fr.wcs.foodtruck.R;
@@ -15,6 +18,7 @@ public class HoraireActivity extends AppCompatActivity {
     private RadioButton radioMatin,radioSoir,radioPeinTemps,radioQuinze,radioTrente,radioAllDay,radioOneDay;
     private Spinner spinnerDebutHoraire,spinnerFinHoraire;
     private EditText choixJourHoraire;
+    private RelativeLayout RelativeIntervalle,relativeSpinner,RelativeDayWeek,relativeEditDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +38,26 @@ public class HoraireActivity extends AppCompatActivity {
         radioTrente =(RadioButton) findViewById(R.id.radioTrente);
         radioAllDay =(RadioButton) findViewById(R.id.radioAllDay);
         radioOneDay =(RadioButton) findViewById(R.id.radioOneDay);
+        RelativeIntervalle = (RelativeLayout) findViewById(R.id.RelativeIntervalle);
+        relativeSpinner = (RelativeLayout)findViewById(R.id.relativeSpinner);
+        RelativeDayWeek = (RelativeLayout)findViewById(R.id.RelativeDayWeek);
+        relativeEditDay = (RelativeLayout)findViewById(R.id.relativeEditDay);
+
+        radioGroupHoraire.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radioMatin:
+                        relativeSpinner.setVisibility(View.VISIBLE);
+                        break;
+                    case R.id.radioSoir:
+                        relativeSpinner.setVisibility(View.VISIBLE);
+                        break;
+                    case R.id.radioPleinTemps:
+                        relativeSpinner.setVisibility(View.VISIBLE);
+                        break;
+                }
+            }
+        });
     }
 }
